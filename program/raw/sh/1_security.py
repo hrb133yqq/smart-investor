b3c7b86e3337d1e279ddeb28201f23630639f99e
BEIJING\sawyeryang <sawyeryang@objectivasoftware.com>
2018-09-03 08:59:41 +0800

    add 1_security

---
+++ program/raw/sh/1_security.py
@@ -0,0 +1,36 @@
+import sys
+reload(sys)
+sys.setdefaultencoding('utf-8')
+
+url_shA="http://query.sse.com.cn/security/stock/downloadStockListFile.do?csrcCode=&stockCode=&areaName=&stockType=1"
+url_shB="http://query.sse.com.cn/security/stock/downloadStockListFile.do?csrcCode=&stockCode=&areaName=&stockType=2"
+raw_sh = "../../../data/raw/sh/"
+
+download_headers={
+    'Accept':'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding':'gzip, deflate',
+    'Host':'query.sse.com.cn',
+    'Referer':'http://www.sse.com.cn/assortment/stock/list/share',
+    'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/68.0.3440.106 Safari/537.36'
+}
+
+import os.path
+import requests
+
+def download(url, headers, saveTo):
+    if os.path.isfile(saveTo):
+        print "File exists:", saveTo
+    else:
+        response = requests.get(url, headers=headers)
+        file = open(saveTo, "w")
+        file.write(response.content)
+        file.close()
+        print "File created:", saveTo
+
+url_file_arr = [
+    {'url': url_shA, 'file':raw_sh+"1_security/shA.xls"},
+    {'url': url_shB, 'file':raw_sh+"1_security/shB.xls"}
+]
+
+for url_file in url_file_arr:
+    download(url_file['url'], download_headers, url_file['file'])

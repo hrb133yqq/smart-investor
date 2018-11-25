b573ef2ab7ee549a9d3d7ec8196b692336b509a9
BEIJING\sawyeryang <sawyeryang@objectivasoftware.com>
2018-11-25 16:18:24 +0800

    include current price

--- program/refined/sh/5_fill_price.py
+++ program/refined/sh/5_fill_price.py
@@ -12,9 +12,11 @@ filePattern = raw_sh + "5_price/*.json"
 jsons = local_functions.read_json_files(filePattern)
 
 infos={} # {'sec code':[info]}
+cur_infos={} # {'sec code':[info]}
 for j in jsons:
-    if len(j['result'])>1:
+    if len(j['result'])==4:
         code = j['result'][1]['id']
+
         closeDate = j['result'][1]['closeTxDate']
         closePrice = j['result'][1]['closePrice']
         info = {'closeDate':closeDate, 'closePrice':closePrice}
@@ -22,5 +24,11 @@ for j in jsons:
             infos[code].append(info)
         else:
             infos[code]=[info]
+
+        cur_closeDate = j['result'][3]['closeTxDate']
+        cur_closePrice = j['result'][3]['closePrice']
+        if code not in cur_infos:
+            cur_infos[code]={'closeDate':cur_closeDate, 'closePrice':cur_closePrice}
+            infos[code].append(cur_infos[code])
 
 securitydb.fill_price_info(infos)
